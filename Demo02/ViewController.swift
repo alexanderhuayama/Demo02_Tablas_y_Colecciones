422a9a387365222114b0a9aab54951b93c55e14f
Giovanni Alexander <alexander.hh.cc@gmail.com>
2017-10-13 12:40:57 -0700

    Initial Commit

---
+++ Demo02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo02
+//
+//  Created by Giovanni on 10/13/17.
+//  Copyright © 2017 Giovanni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
